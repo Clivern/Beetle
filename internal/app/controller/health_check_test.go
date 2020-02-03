4ee51de42dcb06e8117eebac203ac8a201ab97e0
Clivern <hello@clivern.com>
2020-02-03 21:08:43 +0100

    update testing

--- internal/app/controller/health_check_test.go
+++ internal/app/controller/health_check_test.go
@@ -2,26 +2,30 @@
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
-package main
+package controller
 
 import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/clivern/beetle/internal/app/module"
 
 	"github.com/drone/envsubst"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
 var testingConfig = "config.testing.yml"
 
-// TestMain test cases
-func TestMain(t *testing.T) {
+// TestHealthCheck test cases
+func TestHealthCheck(t *testing.T) {
 	// LoadConfigFile
 	t.Run("LoadConfigFile", func(t *testing.T) {
 		fs := module.FileSystem{}
@@ -46,9 +50,33 @@ func TestMain(t *testing.T) {
 
 		got := viper.GetString("app.mode")
 		want := "test"
-
 		if got != want {
 			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	// TestHealthCheckController
+	t.Run("TestHealthCheckController", func(t *testing.T) {
+		router := gin.Default()
+
+		router.GET("/_healthcheck", HealthCheck)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/_healthcheck", nil)
+		router.ServeHTTP(w, req)
+
+		got1 := w.Code
+		want1 := 200
+
+		if got1 != want1 {
+			t.Errorf("got %v, want %v", got1, want1)
+		}
+
+		got2 := strings.TrimSpace(w.Body.String())
+		want2 := `{"status":"ok"}`
+
+		if got2 != want2 {
+			t.Errorf("got %v, want %v", got2, want2)
 		}
 	})
 }
