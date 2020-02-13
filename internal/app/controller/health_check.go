aec43911578f6060b79c02bcd80027d694a14e12
Clivern <hello@clivern.com>
2020-02-13 23:03:38 +0100

    update

--- internal/app/controller/health_check.go
+++ internal/app/controller/health_check.go
@@ -5,14 +5,36 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/clivern/beetle/internal/app/module"
+
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 // HealthCheck controller
 func HealthCheck(c *gin.Context) {
+	logger, _ := module.NewLogger(
+		viper.GetString("log.level"),
+		viper.GetString("log.format"),
+		[]string{viper.GetString("log.output")},
+	)
+
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	status := "ok"
+
+	logger.Info(fmt.Sprintf(
+		`Health Status: %s`,
+		status,
+	), zap.String("CorrelationId", c.Request.Header.Get("X-Correlation-ID")))
+
 	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
+		"status": status,
 	})
 }
