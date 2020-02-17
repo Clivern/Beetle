c1d5b176ef625a7558016eae9ac01d6947ac2122
Clivern <hello@clivern.com>
2020-02-17 21:42:03 +0100

    db module

--- internal/app/module/database.go
+++ internal/app/module/database.go
@@ -4,10 +4,53 @@
 
 package module
 
-// Database struct
-type Database struct{}
+import (
+	"database/sql"
+	"fmt"
 
-// Connect Ccnnects to a MySQL database
-func (db *Database) Connect() (bool, error) {
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Database struct
+type Database struct {
+	Connection *sql.DB
+}
+
+// DSN struct
+type DSN struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Hostname string `json:"hostname"`
+	Port     int    `json:"port"`
+	Database string `json:"database"`
+}
+
+// ToString gets the dsn string
+func (dsn *DSN) ToString() string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s",
+		dsn.Username,
+		dsn.Password,
+		dsn.Hostname,
+		dsn.Port,
+		dsn.Database,
+	)
+}
+
+// Connect connects to a MySQL database
+func (db *Database) Connect(dsn DSN) (bool, error) {
+	var err error
+
+	db.Connection, err = sql.Open("mysql", dsn.ToString())
+
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
+}
+
+// Close closes MySQL database connection
+func (db *Database) Close() error {
+	return db.Connection.Close()
 }
