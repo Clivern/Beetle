223a5afcf4fdabddeceaa2462ac2eb495dc62c03
Clivern <hello@clivern.com>
2020-02-22 17:23:02 +0100

    #31 - ui change

--- public/src/app/app.component.spec.ts
+++ public/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'beetle'`, () => {
+  it(`should have as title 'Beetle'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('beetle');
+    expect(app.title).toEqual('Beetle');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('beetle app is running!');
+    expect(compiled.querySelector('.content p').textContent).toContain('Kubernetes');
   });
 });
