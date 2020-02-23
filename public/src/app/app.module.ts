c5406cd4ba102777c8faa69b232f0ae4cc1885a9
Clivern <hello@clivern.com>
2020-02-23 14:06:02 +0100

    #31 - ui element fix

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SelectComponent } from './select/select.component';
+import { SelectComponent } from './elements/select/select.component';
 
 @NgModule({
   declarations: [
