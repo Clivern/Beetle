6d0acf4173eae057b348b9187f4f7ef39024d7ea
Clivern <hello@clivern.com>
2020-02-23 14:00:42 +0100

    #31 - ui element

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SelectComponent
   ],
   imports: [
     BrowserModule,
