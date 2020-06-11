9adf513aeb91be47452eb10e628e766529aa4040
Clivern <hello@clivern.com>
2020-06-12 00:28:04 +0200

    add recreate strategy deployment 😌 #29

--- internal/app/kubernetes/deployment_strategy.go
+++ internal/app/kubernetes/deployment_strategy.go
@@ -5,9 +5,12 @@
 package kubernetes
 
 import (
+	"context"
 	"fmt"
+	"strings"
 
 	"github.com/clivern/beetle/internal/app/model"
+	"github.com/clivern/beetle/internal/app/util"
 )
 
 // Deploy deploys an application
@@ -33,10 +36,95 @@ func (c *Cluster) Deploy(deploymentRequest model.DeploymentRequest) (bool, error
 }
 
 // RecreateStrategy terminates the old version and release the new one.
+// This method is like running this command
+//
+// ```
+//    	$ kubectl patch deployment toad-deployment --type=json -p '[
+// 	  	  	{"op":"replace", "path":"/spec/strategy", "value":{"type":"Recreate"}},
+//   		{"op":"replace","path":"/spec/template/spec/containers/0/image","value":"clivern/toad:release-0.2.3"}
+//     	]'
+// ```
+// In order to use it
+//
+//```
+// 		fmt.Println(cluster.RecreateStrategy(model.DeploymentRequest{
+//			Cluster:     $$cluster,
+//			Namespace:   $$namespace,
+//			Application: $$application_id,
+//			Version:     $$application_new_version,
+//			Strategy:    "recreate",
+//		}))
+// ```
 func (c *Cluster) RecreateStrategy(deploymentRequest model.DeploymentRequest) (bool, error) {
-	// deploymentRequest.Namespace
-	// deploymentRequest.Application
-	// deploymentRequest.Version
+	result := Application{}
+	patch := make(map[string][]model.PatchStringValue)
+
+	config, err := c.GetConfig(context.Background(), deploymentRequest.Namespace)
+
+	if err != nil {
+		return false, err
+	}
+
+	for _, app := range config.Applications {
+		if app.ID == deploymentRequest.Application {
+			result, err = c.GetApplication(
+				context.Background(),
+				deploymentRequest.Namespace,
+				app.ID,
+				app.Name,
+				app.ImageFormat,
+			)
+			if err != nil {
+				return false, err
+			}
+			break
+		}
+	}
+
+	i := 0
+	for _, container := range result.Containers {
+		if _, ok := patch[container.Deployment.Name]; !ok {
+			patch[container.Deployment.Name] = []model.PatchStringValue{}
+		}
+		patch[container.Deployment.Name] = append(patch[container.Deployment.Name], model.PatchStringValue{
+			Op:    "replace",
+			Path:  fmt.Sprintf("/spec/template/spec/containers/%d/image", i),
+			Value: strings.Replace(container.Image, container.Version, deploymentRequest.Version, -1),
+		})
+		i += 1
+	}
+
+	data := ""
+	status := true
+
+	for deployment_name, deployment_patch := range patch {
+		data, err = util.ConvertToJSON(deployment_patch)
+
+		if err != nil {
+			return false, err
+		}
+
+		// Enforce Recreate strategy
+		data = strings.Replace(
+			data,
+			`[`, `[{"op":"replace","path":"/spec/strategy","value":{"type":"Recreate"}},`,
+			-1,
+		)
+
+		status, err = c.PatchDeployment(
+			context.Background(),
+			deploymentRequest.Namespace,
+			deployment_name,
+			data,
+		)
+
+		if !status || err != nil {
+			return false, err
+		}
+	}
+
+	// TODO -----> watch deployment status to ensure it is fully succeeded
+
 	return true, nil
 }
 
